f66f5d8efcb3820465dab5c9348f88103e64576d
Эдуард <Edyard911@yandex.ru>
2022-08-19 09:24:27 +0400

    Внес исправления по видео из ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,76 @@
 public class Main {
     public static void main(String[] args) {
+        task1 (); task2 (); task3 (); task4 (); task5 ();
+    }
+    public static void task1 () {
         byte a = 2;
         short b = 3333;
         int c = 444444;
         long d = 555555555L;
+
         float f = 25.75f;
         double j = 1.2000000;
 
+        boolean i = true;
+        char ch = 'n';
+    }
+    public static void task2 () {
         float boxerFirst = 78.2f;
         float boxerSecond = 82.7f;
         float total =  boxerFirst + boxerSecond;
-        System.out.println ("Общий вес боксеров = " + total + "кг.");
         float totalWeight =  boxerSecond - boxerFirst;
+        System.out.println ("Общий вес боксеров = " + total + "кг.");
         System.out.println ("Разница между весами боксеров = " + totalWeight + "кг.");
-
-        short bananas = 5 * 80;
-        short milk = 105 + 105;
-        short iceCream = 2 * 200;
-        short eggs = 4 * 70;
-        int sportsBreakfast = bananas + milk + iceCream + eggs;
-        System.out.println ("Общий вес спорт-завтрака = " + sportsBreakfast + "г.");
-        double sportsBreakfastKg = sportsBreakfast / 1000f;
-        System.out.println ("Общий вес спорт-завтрака = " + sportsBreakfastKg + "кг.");
-
-        short slowWeightLoss = 7000 / 250;
-        System.out.println ("При похудении на 250г в день, для достижения результата 7кг понадобится " + slowWeightLoss + " дней ");
-        short fastWeightLoss = 7000 / 500;
-        System.out.println ("При похудении на 500г в день, для достижения результата 7кг понадобится " + fastWeightLoss + " дней ");
-        int averageWeightLoss = (slowWeightLoss + fastWeightLoss) / 2;
-        System.out.println ("В среднем, для достижения результата 7кг понадобится " + averageWeightLoss + " день ");
-
-        float Masha = 67760 + 67760.0f / 100 * 10;
-        float annualIncreaseMasha = 12 * 67760.0f / 100 * 10;
-        System.out.println ("Маша теперь получает " + Masha + "р. Годовой доход вырос на " + annualIncreaseMasha + "р.");
-        float Denis = 83690 + 83690.0f / 100 * 10;
-        float annualIncreaseDenis = 12 * 83690.0f / 100 * 10;
-        System.out.println ("Денис теперь получает " + Denis + "р. Годовой доход вырос на " + annualIncreaseDenis + "р.");
-        float Christina = 76230 + 76230.0f / 100 * 10;
-        float annualIncreaseChristina = 12 * 76230.0f / 100 * 10;
-        System.out.println ("Кристина теперь получает " + Christina + "р. Годовой доход вырос на " + annualIncreaseChristina + "р.");
-
-
-
-
-
-
-
-
-
     }
+    public static void task3 () {
+        int bananas = 5 * 80;
+        int milk = 2 * 105;
+        int iceCream = 2 * 100;
+        int eggs = 4 * 70;
+        int sportsBreakfast = bananas + milk + iceCream + eggs;
+        int gramsInKg = 1000;
+        double sportsBreakfastKg = sportsBreakfast / (gramsInKg * 1.0);
+        System.out.println ("Общий вес спорт-завтрака = " + sportsBreakfastKg + "кг.");
+    }
+    public static void task4 () {
+        int weight = 7;
+        int gramsInKilogram = 1000;
+        int totalNumberOfGrams = weight * gramsInKilogram;
+        int minGramsPerDay = 250;
+        int maxGramsPerDay = 500;
+        int slowWeightLoss = totalNumberOfGrams / minGramsPerDay;
+        int fastWeightLoss = totalNumberOfGrams / maxGramsPerDay;
+        int averageWeightLoss = (slowWeightLoss + fastWeightLoss) / 2;
+        System.out.println ("При похудении на 250г в день, для достижения результата 7кг понадобится " + slowWeightLoss + " дней ");
+        System.out.println ("При похудении на 500г в день, для достижения результата 7кг понадобится " + fastWeightLoss + " дней ");
+        System.out.println ("В среднем, для достижения результата 7кг понадобится " + averageWeightLoss + " день ");
+    }
+    public static void task5 () {
+        int percent = 10;
+        double multiplayer = percent / (100 * 1.0);
+        int Masha = 67_760;
+        int Denis = 83_690;
+        int Christina = 76_230;
+
+        int MashaNewSalary = (int) (Masha + (Masha * multiplayer));
+        int DenisNewSalary = (int) (Denis + (Denis * multiplayer));
+        int ChristinaNewSalary = (int) (Christina + (Christina * multiplayer));
+
+        int annualIncreaseMasha = (MashaNewSalary - Masha) * 12;
+        int annualIncreaseDenis = (DenisNewSalary - Denis) * 12;
+        int annualIncreaseChristina = (ChristinaNewSalary - Christina) * 12;
+
+        System.out.println ("Маша теперь получает " + MashaNewSalary + "р. Годовой доход вырос на " + annualIncreaseMasha + "р.");
+        System.out.println ("Денис теперь получает " + DenisNewSalary + "р. Годовой доход вырос на " + annualIncreaseDenis + "р.");
+        System.out.println ("Кристина теперь получает " + ChristinaNewSalary + "р. Годовой доход вырос на " + annualIncreaseChristina + "р.");
+    }
+
+
+
+
+
+
+
+
+
 }
